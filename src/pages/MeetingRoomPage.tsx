9aaef765ea4e40d84043e867bb22b220430cfa89
sadranoras <norassadra1388@gmail.com>
2026-08-01 00:26:16 +0330

    Update MeetingRoomPage.tsx

--- src/pages/MeetingRoomPage.tsx
+++ src/pages/MeetingRoomPage.tsx
@@ -39,7 +39,12 @@ export default function MeetingRoomPage({ roomId, roomCode, participantId, onLea
 
   // 🟢 ارسال همزمان استریم وب‌کم و اسکرین به WebRTC
   const currentUserId = me?.id || '';
-  const { remoteStreams, connectToUser } = useWebRTC(currentUserId, media.camStream, media.screenStream);
+  const { remoteStreams, connectToUser } = useWebRTC(
+  currentUserId,
+  media.camStream,
+  media.micStream,
+  media.screenStream
+);
 
   useEffect(() => {
     if (!currentUserId || participants.length === 0) return;
